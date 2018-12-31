f9da713bc8d8d7538146e8f40ff7379de99f8d38
Danny Diekroeger <danny@bitgo.com>
2018-12-31 08:16:10 -0800

    fixed $ sign in bash script

--- krs-sign.sh
+++ krs-sign.sh
@@ -1,4 +1,4 @@
 ./bin/bitgo krsrecovery -f $1 -r $2
 xprv=$(cat .secret-file | jq -r '.prv')
-../key-recovery-service-v2/bin/admin.js sign --key xprv $2
+../key-recovery-service-v2/bin/admin.js sign --key $xprv $2
 rm .secret-file
